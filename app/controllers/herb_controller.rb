ea1e1b6002db7cb0c8bf5267d7e19da278a3a687
Sarah Proctor <sproctor950@gmail.com>
2022-04-28 09:56:49 -0700

    adds create and new routes

--- app/controllers/herb_controller.rb
+++ app/controllers/herb_controller.rb
@@ -8,4 +8,22 @@ class HerbController < ApplicationController
     @herb = Herb.find(params[:id])
   end
 
+  def new
+    @herb = Herb.new
+  end
+
+  def create
+    @herb = Herb.create(herb_params)
+    if @herb.valid?
+      redirect_to herbs_path
+    end
+  end
+
+  # anything below this line can only be accessed inside the scope of this class
+  private
+  # strong params:
+  def herb_params
+    params.require(:herb).permit(:name, :watered)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'herb' => 'herb#index', as: 'herbs'
+  get 'herb/new' => 'herb#new', as: 'new_herb'
   get 'herb/:id' => 'herb#show', as: 'herb'
+  post 'herb' => 'herb#create'
   # root "articles#index"
 end
